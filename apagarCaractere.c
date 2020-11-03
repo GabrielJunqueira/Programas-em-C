322876cb90ae4f511838b4c156817946e2028e09
GabrielJunqueira <64487574+GabrielJunqueira@users.noreply.github.com>
2020-11-03 20:39:45 -0300

    Create apagarCaractere.c

---
+++ apagarCaractere.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+vezes(char *p, char r){
+    int n = 0;
+    for(int i = 0; i < strlen(p); i++){
+       if (p[i] != r)
+       printf("%c" , p[i]);
+       n++;
+    }    
+}
+    
+    
+int main(){
+    char palavra[40];
+    char letra;
+    
+    scanf("%s" , palavra);
+    getchar();
+    scanf ("%c" , &letra);
+    
+    vezes(palavra , letra);
+}
